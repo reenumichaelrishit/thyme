ba780c9415c3e0f579d33730773bd2c14f2cce37
rishitgupta <rishit@theguptafamily.com>
2025-02-20 14:46:04 -0800

    initialised design tokens & global styles

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,20 @@
-import Nav from './Nav'
+import styled from 'styled-components'
 import { Outlet } from 'react-router-dom'
+import Nav from './Nav'
+
+const GlobalStyleContainer = styled.div`
+    min-height: 100vh;
+    color: ${p => p.theme.color.green.default};
+    background-color: ${p => p.theme.color.black.default};
+    font-weight: ${p => p.theme.weight.regular};
+    font-family: ${p => p.theme.font.content};
+`
 
 const Layout = () => (
-  <div>
-    <Nav />
-    <Outlet />
-  </div>
+    <GlobalStyleContainer>
+        <Nav />
+        <Outlet />
+    </GlobalStyleContainer>
 )
 
 export default Layout
--- src/index.tsx
+++ src/index.tsx
@@ -4,26 +4,30 @@ import {createBrowserRouter, RouterProvider } from "react-router-dom"
 import Layout from "./components/Layout"
 import Home from "./components/Home"
 import About from "./components/About"
+import { ThemeProvider } from "styled-components"
+import theme from "./tokens"
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Home />
-      },
-      {
-        path: "/about",
-        element: <About />
-      }
-    ]
-  }
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <Home />
+            },
+            {
+                path: "/about",
+                element: <About />
+            }
+        ]
+    }
 ])
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <ThemeProvider theme={theme}>
+            <RouterProvider router={router} />
+        </ThemeProvider>
+    </React.StrictMode>
 )
