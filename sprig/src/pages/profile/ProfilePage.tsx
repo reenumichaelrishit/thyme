5d5d6d0224bd013bb55951f4ad0029a3ec872cb9
Michael <mvmonte12@gmail.com>
2025-04-15 11:09:25 -0700

    Finishing touches on Profile Page

--- sprig/src/pages/profile/ProfilePage.tsx
+++ sprig/src/pages/profile/ProfilePage.tsx
@@ -1,5 +1,9 @@
 import styled from "styled-components"
 import PhotoWindow from "../../components/PhotoWindow";
+import { ReactNode, useRef } from "react"
+import Scrollbars from "react-custom-scrollbars-2"
+import { CaretCircleLeft, CaretCircleRight } from "@phosphor-icons/react"
+import ScrollContainer from "../../components/ScrollContainer";
 const Grids = styled.div `
     min-height: 90vh;
     display: grid;
@@ -21,10 +25,57 @@ const Description = styled.div `
     justify-self: center;
 `
 
+const HorizontalScrollContainer = (props: { height: string, scrollOffset: number, children: ReactNode }) => {
+    const scrollRef = useRef(null)
+
+    const getScrollWidth = () => (scrollRef.current?.getScrollLeft())
+    const scrollToLeft = () => scrollRef.current?.scrollLeft(getScrollWidth() - props.scrollOffset)
+    const scrollToRight = () => scrollRef.current?.scrollLeft(getScrollWidth() + props.scrollOffset)
+
+    return (
+        <div style={{ display: "flex", alignItems: "center" }}>
+            <CaretCircleLeft size={48} onClick={scrollToLeft} />
+            <Scrollbars
+                style={{ width: "100%", height: props.height }}
+                ref={scrollRef}
+                renderThumbVertical={() => <div style={{ display: "none" }} />}
+                renderThumbHorizontal={() => <div style={{ display: "none" }} />}
+            >
+                <div style={{ display: "flex", width: "100%", height: "100%", alignItems: "center" }}>
+                    {props.children}
+                </div>
+            </Scrollbars>
+            <CaretCircleRight size={48} onClick={scrollToRight} />
+        </div>
+    )
+}
+
+const TestDiv = styled.div `
+    display: inline-block;
+    min-width: 19vw;
+`
+
+const SavedPosts = styled.div `
+    grid-column: 1/4;
+    grid-row: 3;
+    // grid-column-gap: 10px
+`
+const UserPosts = styled.div `
+    grid-column: 1/4;
+    grid-row: 2;
+    grid-column-gap: 10px
+`
+
+const FriendsButton = styled.div `
+    grid-column: 3;
+    grid-row 1;
+`
+
 const ProfilePage = (props: {
     ownProfile: boolean;
     profilePic?: string;
     }) => (
+        <ScrollContainer>
         <Grids>
             <img src={props.profilePic ?? "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"}
                  style= {{
@@ -40,13 +91,45 @@ const ProfilePage = (props: {
                 <h3>
                     This is a description that is much longer than I have seen previously let's see how it
                 </h3>
-                <button 
-                    style={{border: "none", background: "none"}}>
-                    <u>edit</u>
-                </button>
+                {props.ownProfile ?
+                    <button 
+                        style={{border: "none", background: "none"}}>
+                        <u>edit</u>
+                    </button>: <div/>}
             </Description>
-            <PhotoWindow title = {"Some Recipe"} />
+
+            <UserPosts>
+                {props.ownProfile ? <h2 style={{alignSelf: "center", justifySelf: "center"}}>Your Posts</h2> :
+                                <h2 style={{alignSelf: "center", justifySelf: "center"}}>User's Posts</h2>}
+                <HorizontalScrollContainer height={"40vh"}
+                                        scrollOffset={200}>
+                    <TestDiv><PhotoWindow title = {"Some Recipe"} /></TestDiv>
+                    <TestDiv><PhotoWindow title = {"Some Recipe"} /></TestDiv>
+                    <TestDiv><PhotoWindow title = {"Some Recipe"} /></TestDiv>
+                    <TestDiv><PhotoWindow title = {"Some Recipe"} /></TestDiv>
+                    <TestDiv><PhotoWindow title = {"Some Recipe"} /></TestDiv>
+                    <TestDiv><PhotoWindow title = {"Some Recipe"} /></TestDiv>
+                    <TestDiv><PhotoWindow title = {"Some Recipe"} /></TestDiv>
+                    <TestDiv><PhotoWindow title = {"Some Recipe"} /></TestDiv>
+                </HorizontalScrollContainer>
+            </UserPosts>
+            {props.ownProfile ?
+            <SavedPosts>
+                <h2 style={{alignSelf: "center", justifySelf: "center"}}>Saved Posts</h2>
+                <HorizontalScrollContainer height={"40vh"}
+                                        scrollOffset={200}>
+                    <TestDiv><PhotoWindow title = {"Some Recipe"} /></TestDiv>
+                    <TestDiv><PhotoWindow title = {"Some Recipe"} /></TestDiv>
+                    <TestDiv><PhotoWindow title = {"Some Recipe"} /></TestDiv>
+                    <TestDiv><PhotoWindow title = {"Some Recipe"} /></TestDiv>
+                    <TestDiv><PhotoWindow title = {"Some Recipe"} /></TestDiv>
+                    <TestDiv><PhotoWindow title = {"Some Recipe"} /></TestDiv>
+                    <TestDiv><PhotoWindow title = {"Some Recipe"} /></TestDiv>
+                    <TestDiv><PhotoWindow title = {"Some Recipe"} /></TestDiv>
+                </HorizontalScrollContainer>
+            </SavedPosts> : <div /> }
         </Grids>
+        </ScrollContainer>
     )
     
 
