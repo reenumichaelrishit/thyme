25dcdbb41a33b269812123802f06d929ce7395ae
rishitgupta <rishit@theguptafamily.com>
2025-05-08 19:07:52 -0700

    implemented CORS

--- roots/src/index.ts
+++ roots/src/index.ts
@@ -1,8 +1,11 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import path from "path";
-import {createClient} from '@supabase/supabase-js'
-import {registerAuthRoutes} from "./routes/auth";
+import { createClient } from '@supabase/supabase-js'
+import { registerAuthRoutes } from "./routes/auth";
+import { registerPostRoutes } from "./routes/posts";
+
+const cors = require("cors");
 
 dotenv.config(); // Read the .env file in the current working directory, and load values into process.env.
 const PORT = process.env.PORT || 3000;
@@ -21,8 +24,14 @@ const supabase = createClient(sbUrl, SB_SERVICE_ROLE_KEY ?? "", {
 const app = express();
 app.use(express.json());
 app.use(express.static(staticDir));
+app.use(cors({
+    origin: process.env.FRONTEND_SERVER,
+    methods: ["GET", "POST"],
+    credentials: true
+}))
 
 registerAuthRoutes(app, supabase);
+registerPostRoutes(app, supabase);
 
 app.get("/hello", (req: Request, res: Response) => {
     res.send("Hello, World");
